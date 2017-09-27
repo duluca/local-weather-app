eba5627b55486afff250b50195e5e329163f5eb6
Doguhan Uluca <duluca@gmail.com>
2017-09-27 09:21:24 -0400

    updated line length

--- web-app/src/app/current-weather/current-weather.component.ts
+++ web-app/src/app/current-weather/current-weather.component.ts
@@ -13,7 +13,8 @@ export class CurrentWeatherComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
-    this.weatherService.getCurrentWeather('Bethesda', 'US')
-      .subscribe(data => this.current = data )
+    this.weatherService
+      .getCurrentWeather('Bethesda', 'US')
+      .subscribe(data => (this.current = data))
   }
 }
