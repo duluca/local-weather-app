37e73169a8413582666f140dbce8d19ecb7487a1
Doguhan Uluca <duluca@gmail.com>
2019-04-15 02:01:47 -0400

    reconfigured now

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-import { async, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { CitySearchComponent } from './city-search/city-search.component';
-import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-import { MaterialModule } from './material.module';
-import { WeatherService } from './weather/weather.service';
-import { WeatherServiceFake } from './weather/weather.service.fake';
+import { TestBed, async } from '@angular/core/testing'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
+import { AppComponent } from './app.component'
+import { CitySearchComponent } from './city-search/city-search.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { MaterialModule } from './material.module'
+import { WeatherService } from './weather/weather.service'
+import { WeatherServiceFake } from './weather/weather.service.fake'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/app/city-search-tpldriven/city-search-tpldriven.component.ts
+++ src/app/city-search-tpldriven/city-search-tpldriven.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
-import { WeatherService } from '../weather/weather.service';
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-city-search-tpldriven',
