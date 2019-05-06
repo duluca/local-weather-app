589976762217cad0b6cf22e72ab0546fd016b45e
brendoncaulkins <brendon.caulkins@excella.com>
2019-05-06 11:42:47 -0400

    Closer replication of existing tests in nobed example

--- src/app/current-weather/current-weather.component.nobed.spec.ts
+++ src/app/current-weather/current-weather.component.nobed.spec.ts
@@ -1,11 +1,9 @@
 import { Store } from '@ngrx/store'
-import { MockStore, provideMockStore } from '@ngrx/store/testing'
 import { addPropertyAsBehaviorSubject } from 'angular-unit-test-helper'
 import { of } from 'rxjs'
 
-import { ICurrentWeather } from '../interfaces'
 import { AppState } from '../reducers'
-import { WeatherService, defaultWeather } from '../weather/weather.service'
+import { WeatherService } from '../weather/weather.service'
 import { fakeWeather } from '../weather/weather.service.fake'
 import { CurrentWeatherComponent } from './current-weather.component'
 
@@ -16,7 +14,7 @@ import { CurrentWeatherComponent } from './current-weather.component'
 //    - Lifecycle hooks must be called manually
 //    - Lifecycle helper funcs and funcs/getters called by HTML aren't called magically
 //    - Which yields more accurate code coverage data
-//    - Runs ~10x faster (0.037s, compared to 0.44s with the TestBed)
+//    - Runs ~8-10x faster (0.066s, compared to 0.571s with the TestBed)
 //    - Has fewer imports / requires less maintenance
 //    - Forces higher knowledge of how the lifecycle interactes with your component
 //    - Allows you to better separate Unit (fast) and Integration (slow) tests
@@ -28,34 +26,86 @@ describe('CurrentWeatherComponent (no TestBed)', () => {
   let component: CurrentWeatherComponent
   let weatherServiceMock: jasmine.SpyObj<WeatherService>
   let store: jasmine.SpyObj<Store<AppState>>
-  const initialState = { search: { current: defaultWeather } }
 
   beforeEach(() => {
-    weatherServiceMock = jasmine.createSpyObj('WeatherService', ['getCurrentWeather'])
+    weatherServiceMock = jasmine.createSpyObj(WeatherService.name, ['getCurrentWeather'])
     addPropertyAsBehaviorSubject(weatherServiceMock, 'currentWeather$')
+
+    // ngrx data store must be created manually
     store = jasmine.createSpyObj('AppState Store', ['pipe'])
     store.pipe.and.returnValue(of())
-
-    // Create the class under test manually
-    component = new CurrentWeatherComponent(weatherServiceMock, store)
   })
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    // After migrating to ngrx, everything happens in the constructor...
+    //  ... which is normally called by fixture.detectChanges() ...
+    //  ... so that action is up to us to call manually as our Act
+    component = new CurrentWeatherComponent(weatherServiceMock, store)
+
+    // Assert
     expect(component).toBeTruthy()
   })
 
-  it('should get currentWeather from weatherService', () => {
+  // When testing data inside a subscribe block, using a callback like 'done'...
+  // ...ensures the code inside get called, and causes an AsyncTimeout if not
+  // Jasmine tests pass by default, so ensuring the expect(s) are called is critical
+  it('should get currentWeather from weatherService', done => {
     // Arrange
     weatherServiceMock.currentWeather$.next(fakeWeather)
 
     // Act
-    component.ngOnInit() // You have to call lifecycle hooks manually
+    component = new CurrentWeatherComponent(weatherServiceMock, store)
 
     // Assert
     expect(component.current$).toBeDefined()
     component.current$.subscribe(current => {
-      expect(current.city).toEqual('Bethesda')
-      expect(current.temperature).toEqual(280.32)
+      if (current) {
+        expect(current.city).toEqual('Bethesda')
+        expect(current.temperature).toEqual(280.32)
+        done()
+      }
+    })
+  })
+
+  // Disabled with migration to ngrx
+  xdescribe('(pre-ngrx)', () => {
+    beforeEach(() => {
+      component = new CurrentWeatherComponent(weatherServiceMock, store)
+    })
+
+    it('should call getCurrentWeather from weatherService', () => {
+      // Arrange
+      weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+      // Act
+      // Lifecycle hooks must be called manually
+      component.ngOnInit()
+
+      // Assert
+      expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1)
+    })
+
+    it('should eagerly load currentWeather in Bethesda from weatherService', done => {
+      // Arrange
+      weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather))
+
+      // Act
+      // Lifecycle hooks must be called manually
+      component.ngOnInit()
+
+      // Assert
+      expect(component.current$).toBeDefined()
+      component.current$.subscribe(current => {
+        if (current) {
+          expect(current.city).toEqual('Bethesda')
+          expect(current.temperature).toEqual(280.32)
+          done()
+        }
+      })
     })
   })
 })
