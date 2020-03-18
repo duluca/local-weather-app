572547115e2b318544364d38663d2b908b7b1d3b
Doguhan Uluca <duluca@gmail.com>
2020-03-18 02:31:57 -0400

    ch3 code samples

--- projects/ch3/src/app/app.module.ts
+++ projects/ch3/src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, CurrentWeatherComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
