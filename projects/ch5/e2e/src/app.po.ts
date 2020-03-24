ee252f9523b99ff29511f6b4aa616cae1ae7ca26
Doguhan Uluca <duluca@gmail.com>
2020-03-24 02:03:27 -0400

    fix pa11y errors

--- projects/ch5/e2e/src/app.po.ts
+++ projects/ch5/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root div h1')).getText() as Promise<string>
+    return element(by.css('app-root mat-toolbar span')).getText() as Promise<string>
   }
 }
