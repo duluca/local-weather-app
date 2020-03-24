33ec81e8a947ba25cc2e7c9e0d37684572b84a84
Doguhan Uluca <duluca@gmail.com>
2020-03-23 20:22:10 -0400

    final ch4 code

--- projects/ch4/e2e/src/app.e2e-spec.ts
+++ projects/ch4/e2e/src/app.e2e-spec.ts
@@ -11,7 +11,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getTitleText()).toEqual('local-weather-app app is running!')
+    expect(page.getTitleText()).toEqual('LocalCast Weather')
   })
 
   afterEach(async () => {
--- projects/ch4/e2e/src/app.po.ts
+++ projects/ch4/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>
+    return element(by.css('app-root div h1')).getText() as Promise<string>
   }
 }
