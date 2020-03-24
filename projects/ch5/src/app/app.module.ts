8ad3dd1c1058ed833481745b0cc97c355a068866
Doguhan Uluca <duluca@gmail.com>
2020-03-23 21:36:38 -0400

    adding material to ch5

--- projects/ch5/src/app/app.module.ts
+++ projects/ch5/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
