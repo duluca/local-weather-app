2ada7ca339e9a4fa55f82473f417f9cf85a368c0
Doguhan Uluca <duluca@gmail.com>
2020-03-25 03:18:26 -0400

    style and lint fix

--- projects/ch6/src/app/app.component.ts
+++ projects/ch6/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core'
 
-import { WeatherService } from './weather/weather.service'
-
 @Component({
   selector: 'app-root',
   template: `
--- projects/ch6/src/app/city-search/city-search.component.spec.ts
+++ projects/ch6/src/app/city-search/city-search.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing'
 
-import { CitySearchComponent } from './city-search.component';
+import { CitySearchComponent } from './city-search.component'
 
 describe('CitySearchComponent', () => {
-  let component: CitySearchComponent;
-  let fixture: ComponentFixture<CitySearchComponent>;
+  let component: CitySearchComponent
+  let fixture: ComponentFixture<CitySearchComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CitySearchComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [CitySearchComponent],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CitySearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CitySearchComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
