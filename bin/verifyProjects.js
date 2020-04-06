a20d13cfc8990284f3fe8d4e92f1a488244fba5e
Doguhan Uluca <duluca@gmail.com>
2020-04-06 18:25:13 -0400

    updated to relative paths, skip ch12

--- bin/verifyProjects.js
+++ bin/verifyProjects.js
@@ -5,33 +5,37 @@ const path = require('path')
 // get projects root
 const root = path.resolve(__dirname, '../projects/')
 
-fs.readdirSync(root).forEach(module => {
+fs.readdirSync(root).forEach((module) => {
   const modulePath = path.join(root, module)
   console.log(modulePath)
 
-  const skipTests = module === 'ch3'
+  if (module !== 'ch12') {
+    // temporarily skip ch12
 
-  const project = ['--project', module]
+    const skipTests = module === 'ch3'
 
-  const build = ['ng', 'build', '--prod'].concat(project)
-  const test = ['ng', 'test', '--watch=false'].concat(project)
-  const e2e = ['ng', 'e2e', module]
+    const project = ['--project', module]
 
-  const npmCommandsArray = skipTests ? [build] : [build, test, e2e]
+    const build = ['ng', 'build', '--prod'].concat(project)
+    const test = ['ng', 'test', '--watch=false'].concat(project)
+    const e2e = ['ng', 'e2e', module]
 
-  npmCommandsArray.forEach(command => {
-    const spawn = cp.spawnSync(
-      /^win/.test(process.platform) ? 'npx.cmd' : 'npx',
-      command,
-      {
-        env: process.env,
-        cwd: modulePath,
-        stdio: 'inherit',
+    const npmCommandsArray = skipTests ? [build] : [build, test, e2e]
+
+    npmCommandsArray.forEach((command) => {
+      const spawn = cp.spawnSync(
+        /^win/.test(process.platform) ? 'npx.cmd' : 'npx',
+        command,
+        {
+          env: process.env,
+          cwd: modulePath,
+          stdio: 'inherit',
+        }
+      )
+
+      if (spawn.status != 0) {
+        throw `Error: npx ${command.join(' ')} failed`
       }
-    )
-
-    if (spawn.status != 0) {
-      throw `Error: npx ${command.join(' ')} failed`
-    }
-  })
+    })
+  }
 })
--- src/app/postal-code/postal-code.service.ts
+++ src/app/postal-code/postal-code.service.ts
@@ -2,7 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { defaultIfEmpty, flatMap } from 'rxjs/operators'
-import { environment } from 'src/environments/environment'
+
+import { environment } from '../../environments/environment'
 
 export interface IPostalCode {
   countryCode: string
