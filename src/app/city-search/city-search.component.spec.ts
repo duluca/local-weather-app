52420a6c547ec394ccd828a2780d00ab69c43bff
Doguhan Uluca <duluca@gmail.com>
2020-04-06 21:37:43 -0400

    fix test

--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -1,35 +1,52 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { Store } from '@ngrx/store'
+import {
+  ObservablePropertyStrategy,
+  autoSpyObj,
+  injectSpy,
+} from 'angular-unit-test-helper'
+import { of } from 'rxjs'
 
-import { MaterialModule } from '../material.module'
 import { WeatherService } from '../weather/weather.service'
-import { WeatherServiceFake } from '../weather/weather.service.fake'
 import { CitySearchComponent } from './city-search.component'
 
 describe('CitySearchComponent', () => {
   let component: CitySearchComponent
   let fixture: ComponentFixture<CitySearchComponent>
 
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
+
   beforeEach(async(() => {
+    const weatherServiceSpy = autoSpyObj(
+      WeatherService,
+      ['currentWeather$'],
+      ObservablePropertyStrategy.BehaviorSubject
+    )
+
     TestBed.configureTestingModule({
       declarations: [CitySearchComponent],
       providers: [
-        { provide: WeatherService, useClass: WeatherServiceFake },
+        { provide: WeatherService, useValue: weatherServiceSpy },
         { provide: Store, useValue: null },
       ],
-      imports: [FormsModule, ReactiveFormsModule, MaterialModule, NoopAnimationsModule],
     }).compileComponents()
+
+    weatherServiceMock = injectSpy(WeatherService)
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CitySearchComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit
+
+    // Assert
     expect(component).toBeTruthy()
   })
 })
