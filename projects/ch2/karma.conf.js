9c6652303cdbd3b86cfb48cea420235e55622fec
Doguhan Uluca <duluca@gmail.com>
2020-04-25 23:01:35 -0400

    fixed karma conf

--- projects/ch2/karma.conf.js
+++ projects/ch2/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/local-weather-app'),
+      dir: require('path').join(__dirname, '../../coverage/ch2'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
