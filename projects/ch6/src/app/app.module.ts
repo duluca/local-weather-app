89f2e6050496dc5cb1c011fc29b8be403ba14066
Doguhan Uluca <duluca@gmail.com>
2023-08-10 11:54:04 -0400

    migrated to ngx-layout

--- projects/ch6/src/app/app.module.ts
+++ projects/ch6/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { FlexLayoutModule } from '@ngbracket/ngx-layout'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
