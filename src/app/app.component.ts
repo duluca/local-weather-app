b7828b83125f58445bb29a8c988856ea8f8d2dd3
Doguhan Uluca <duluca@gmail.com>
2023-08-10 11:30:40 -0400

    style fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,9 @@ import { Component } from '@angular/core'
       <div fxFlex></div>
       <mat-card appearance="outlined" fxFlex="300px">
         <mat-card-header>
-          <mat-card-title><div class="mat-headline-5">Current Weather</div></mat-card-title>
+          <mat-card-title
+            ><div class="mat-headline-5">Current Weather</div></mat-card-title
+          >
         </mat-card-header>
         <mat-card-content> <app-current-weather></app-current-weather> </mat-card-content>
       </mat-card>
