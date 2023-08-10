93fe715fba13607aa6f33a1acf753020fdfac0c5
Doguhan Uluca <duluca@gmail.com>
2023-08-10 12:10:32 -0400

    Ng15 (#745)

--- src/test.ts
+++ src/test.ts
@@ -8,28 +8,11 @@ import {
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'
 
-// import { ngMocks } from 'ng-mocks'
-
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    keys(): string[]
-    <T>(id: string): T
-  }
-}
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 )
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/)
-// And load the modules.
-context.keys().map(context)
 
 // All methods in mock declarations and providers
 // will be automatically spied on their creation.
