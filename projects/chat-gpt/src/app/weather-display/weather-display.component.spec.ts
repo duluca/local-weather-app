6d4d0ebbf8b2057f4398d654cfa71d2b0f4a68a5
Doguhan Uluca <duluca@gmail.com>
2023-08-23 17:14:08 -0400

    fix style

--- projects/chat-gpt/src/app/weather-display/weather-display.component.spec.ts
+++ projects/chat-gpt/src/app/weather-display/weather-display.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { WeatherDisplayComponent } from './weather-display.component';
+import { WeatherDisplayComponent } from './weather-display.component'
 
 describe('WeatherDisplayComponent', () => {
-  let component: WeatherDisplayComponent;
-  let fixture: ComponentFixture<WeatherDisplayComponent>;
+  let component: WeatherDisplayComponent
+  let fixture: ComponentFixture<WeatherDisplayComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [WeatherDisplayComponent]
-    });
-    fixture = TestBed.createComponent(WeatherDisplayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [WeatherDisplayComponent],
+    })
+    fixture = TestBed.createComponent(WeatherDisplayComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
