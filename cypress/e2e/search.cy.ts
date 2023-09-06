c9a3e8d2084e97a48897165f9abb4dbeba701460
Doguhan Uluca <duluca@gmail.com>
2023-09-06 01:17:33 -0400

    test fix

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -24,18 +24,17 @@ describe('LocalCast Weather', () => {
 function searchByCity(city: string, expectedCity: string) {
   cy.intercept(
     'GET',
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01ff1417eeb4a81b09ac68b15958d453`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01ff1417eeb4a81b09ac68b15958d453`
   ).as('getWeather')
 
   cy.byTestId('search-input').type(`${city}{enter}`)
 
-  cy.wait('@getWeather').then(() => {
-    cy.get('@getWeather').its('request.url').should('contain', `q=${city}`)
-    cy.get('@getWeather').its('response.statusCode').should('eq', 200)
+  cy.wait('@getWeather')
+  cy.get('@getWeather').its('request.url').should('contain', `q=${city}`)
+  cy.get('@getWeather').its('response.statusCode').should('eq', 200)
 
-    cy.byTestId('city').should('contain', expectedCity)
-    cy.byTestId('description').should('not.be.empty')
-    cy.byTestId('temperature').should('not.equal', '0')
-    cy.byTestId('date').should('not.be.empty')
-  })
+  cy.byTestId('city').should('contain', expectedCity)
+  cy.byTestId('description').should('not.be.empty')
+  cy.byTestId('temperature').should('not.equal', '0')
+  cy.byTestId('date').should('not.be.empty')
 }
