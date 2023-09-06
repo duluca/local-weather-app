50a8a90f2be3eafaff4c7ac9162dcf31517ea2ec
Doguhan Uluca <duluca@gmail.com>
2023-09-06 01:45:54 -0400

    lint fix

--- projects/stage12/src/app/city-search/city-search.component.ts
+++ projects/stage12/src/app/city-search/city-search.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgIf } from '@angular/common'
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
