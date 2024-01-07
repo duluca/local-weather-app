339196420ca35daf9e57bde5fbd465a7d4bc11a3
Doguhan Uluca <duluca@gmail.com>
2024-01-07 14:33:08 -0500

    added loading spinner

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,12 @@
-import { provideHttpClient } from '@angular/common/http'
 import { enableProdMode } from '@angular/core'
 import { bootstrapApplication } from '@angular/platform-browser'
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideEffects } from '@ngrx/effects'
-import { provideStore } from '@ngrx/store'
-import { provideStoreDevtools } from '@ngrx/store-devtools'
+import { appConfig } from 'src/app/app.config'
 
 import { AppComponent } from './app/app.component'
-import { CurrentWeatherEffects } from './app/effects/current-weather.effects'
-import { metaReducers, reducers } from './app/reducers'
 import { environment } from './environments/environment'
 
 if (environment.production) {
   enableProdMode()
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideAnimations(), // provideAnimationsAsync(),
-    provideHttpClient(),
-    provideEffects(CurrentWeatherEffects),
-    provideStore(reducers, {
-      metaReducers,
-      runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true },
-    }),
-    provideStoreDevtools({
-      maxAge: 25,
-      logOnly: environment.production,
-      connectInZone: true,
-      // trace: true,
-    }),
-  ],
-}).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
